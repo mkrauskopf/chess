b62504f0c32838ef84fa09ec359e185875845757
Martin Krauskopf <martin.krauskopf@gmail.com>
2023-03-05 18:05:55 +0100

    Add coordinates labels

--- src/main.js
+++ src/main.js
@@ -3,14 +3,43 @@ console.debug('Loading the chess board...')
 const board = document.getElementById('board')
 
 const arr0to7 = [...Array(8).keys()]
-const arr1to8 = arr0to7.map((n) => n + 1)
-const arrAtoH = arr0to7.map((n) => String.fromCharCode(n + 65))
+const rows = arr0to7.map((rowIdx) => rowIdx + 1)
+const columns = arr0to7.map((coldIdx) => String.fromCharCode(coldIdx + 97))
 
-arr1to8.reverse().forEach((row, i) => {
-  arrAtoH.forEach((column, j) => {
-    const field = document.createElement('div')
-    field.classList.add((i + j) % 2 === 0 ? 'white' : 'black')
-    field.appendChild(document.createTextNode(`${column}${row}`))
-    board.appendChild(field)
+const columnToIndex = new Map(arr0to7.map((coldIdx) => [String.fromCharCode(coldIdx + 97), coldIdx]))
+const rowToIndex = new Map(arr0to7.map((rowIdx) => [rowIdx + 1, rowIdx]))
+
+function renderBoard() {
+  rows.reverse().forEach((row) => {
+    columns.forEach((column) => {
+      const square = createSquare(column, row)
+      board.appendChild(square)
+    })
   })
-})
+}
+
+function createSquare(column, row) {
+  const square = document.createElement('div')
+  square.classList.add(
+    'square',
+    (rowToIndex.get(row) + columnToIndex.get(column)) % 2 === 0 ? 'square-black' : 'square-white'
+  )
+
+  if (row === 1) {
+    square.appendChild(createSquareLabel(column, 'row'))
+  }
+  if (column === 'h') {
+    square.appendChild(createSquareLabel(row, 'column'))
+  }
+
+  return square
+}
+
+function createSquareLabel(label, rowOrCol) {
+  const squareLabel = document.createElement('span')
+  squareLabel.classList.add('square-label', `square-label-${rowOrCol}`)
+  squareLabel.appendChild(document.createTextNode(label))
+  return squareLabel
+}
+
+renderBoard()
