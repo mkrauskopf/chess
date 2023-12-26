5ed09d313c19f271548e141ecbe0e5d89d14187d
Martin Krauskopf <martin.krauskopf@gmail.com>
2023-12-26 16:03:16 +0100

    Basic pieces rendering

--- src/main.js
+++ src/main.js
@@ -12,14 +12,43 @@ const rankToIndex = new Map(arr0to7.map((rankIdx) => [rankIdx + 1, rankIdx]))
 function renderBoard() {
   ranks.reverse().forEach((rank) => {
     files.forEach((file) => {
-      const square = createSquare(file, rank)
-      board.appendChild(square)
+      board.appendChild(createSquare(file, rank))
     })
   })
 }
 
+function renderPiece(color, piece, file, rank) {
+  const square = document.getElementById(`${file}${rank}`)
+  square.classList.add(color, piece)
+}
+
+function renderInitialPosition() {
+  // white pieces
+  files.forEach((file) => renderPiece('white', 'pawn', file, 2))
+  renderPiece('white', 'rook', 'a', 1)
+  renderPiece('white', 'knight', 'b', 1)
+  renderPiece('white', 'bishop', 'c', 1)
+  renderPiece('white', 'queen', 'd', 1)
+  renderPiece('white', 'king', 'e', 1)
+  renderPiece('white', 'bishop', 'f', 1)
+  renderPiece('white', 'knight', 'g', 1)
+  renderPiece('white', 'rook', 'h', 1)
+
+  // black pieces
+  files.forEach((file) => renderPiece('black', 'pawn', file, 7))
+  renderPiece('black', 'rook', 'a', 8)
+  renderPiece('black', 'knight', 'b', 8)
+  renderPiece('black', 'bishop', 'c', 8)
+  renderPiece('black', 'queen', 'd', 8)
+  renderPiece('black', 'king', 'e', 8)
+  renderPiece('black', 'bishop', 'f', 8)
+  renderPiece('black', 'knight', 'g', 8)
+  renderPiece('black', 'rook', 'h', 8)
+}
+
 function createSquare(file, rank) {
   const square = document.createElement('div')
+  square.id = `${file}${rank}`
   square.classList.add(
     'square',
     (rankToIndex.get(rank) + fileToIndex.get(file)) % 2 === 0 ? 'square-black' : 'square-white'
@@ -42,4 +71,9 @@ function createSquareLabel(label, rankOrFile) {
   return squareLabel
 }
 
-renderBoard()
+function main() {
+  renderBoard()
+  renderInitialPosition()
+}
+
+main()
